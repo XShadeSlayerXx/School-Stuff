4f9106c645059aa53b132a26e61fea4c97a69955
XShadeSlayerXx <xshadeslayerxx@gmail.com>
2020-11-13 15:31:54 -0600

    lots of fixes

--- assignment 3/assignment3.py
+++ assignment 3/assignment3.py
@@ -8,6 +8,8 @@ dataFile = 'usnewsDistance.txt' or None
 #use this with the other .txt files if you want
 fileToConvert = ''
 
+acceptable_error = .0001
+
 def convertFile(filein, fileout):
     with open(filein, 'r', encoding = 'utf8') as file:
         with open(fileout, 'w', encoding = 'utf8') as outfile:
@@ -51,13 +53,13 @@ def numLines(file):
 
 def pointDistance(array, point1, point2) -> float:
     if point1 > point2:
-        #flip the points and convert point2 to
+        #flip the points and convert point2 to left hand side coordinates
         point1, point2 = point2, point1
-        point2 = point1 - point2 - 1 # ignore the middle line
+    point2 = point2 - point1 - 1 # ignore the middle line
     try:
         return array[point1][point2]
     except:
-        return 1.0
+        raise(Exception)
 
 def pointToLine(array, point):
     total = []
@@ -71,7 +73,6 @@ def pointToLine(array, point):
 
 def SSE(cluster_base, cluster_groups, array):
     errors = []
-    #TODO: iterate through cluster base and the groups together, comparing each cluster's base to the point
     for x in range(len(cluster_base)):
         error_sum = 0
         for y in cluster_groups[x]:
@@ -128,18 +129,22 @@ with open(dataFile) as f:
     for line in f:
         count += 1
         tri_array.append([float(x) for x in line.split(' ') if x != '\n'])
+tri_array = tri_array[:-1]
 
 #random sample to avoid collisions
 clusters = random.sample(range(count), num_points)
 data_clusters = [None] * num_points
-old_clusters = 0
+old_sse = float('inf')
+older_sse = 0
+new_sse = float('-inf')
 too_high = 0
 
 #cluster all the points per cycle
 #choose new center points based on the most well connected point in the cluster to the other points
 
-while clusters != old_clusters or too_high > 20:
-    old_clusters = clusters
+while abs(old_sse - new_sse) > acceptable_error and too_high < 20 and older_sse != new_sse:
+    older_sse = old_sse
+    old_sse = new_sse
     # group the data around the points
     data_clusters = findClusters(clusters, num_points, tri_array)
     # center the points around the most connected data in each cluster
@@ -148,10 +153,8 @@ while clusters != old_clusters or too_high > 20:
     too_high += 1
     print('Iteration num: ',too_high)
     print('Num points per cluster: ',', '.join([str(len(x)) for x in data_clusters]))
-    error = [SSE(clusters, tri_array, x) for x in data_clusters]
-    # print('Error per cluster: ',', '.join([str(x) for x in error]))
-    print('Overall SSE: ', sum([sum(x) for x in error]))
+    error = SSE(clusters, data_clusters, tri_array)
+    print('Error per cluster: ',', '.join([str(x) for x in error]))
+    new_sse = sum(error)
+    print('Overall SSE: ', new_sse)
     print('-------------------\n')
-
-print(too_high)
-print(clusters)
